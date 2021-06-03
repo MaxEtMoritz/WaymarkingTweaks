27e934e6019c9454114d9867eeab4ebf61d759dd
MaxEtMoritz <markus@memotech.franken.de>
2021-06-03 18:55:46 +0200

    Update Waymarking Tweaks.user.js
    added viewport meta

--- Waymarking Tweaks.user.js
+++ Waymarking Tweaks.user.js
@@ -12,9 +12,13 @@
 // ==/UserScript==
 
 var extender = {
-    setup: function(){
+    setup: function () {
+        let meta = document.createElement('meta');
+        meta.name = 'viewport';
+        meta.content = 'width=device-width, height=device-height, initial-scale=1';
+        document.head.append(meta);
         var css = document.createElement('style');
-    css.textContent=`
+        css.textContent = `
     #sidebar.sidebarHidden .gutter {
         display: none;
       }
@@ -140,46 +144,49 @@ var extender = {
       font-weight:normal;
       font-size:inherit;
       }
+      span{
+      display:contents;
+      }
       `;
-    document.head.append(css);
-        if(navigator.geolocation){
+        document.head.append(css);
+        if (navigator.geolocation) {
             //My Location Button(Waymark Search Filter)
-            if(document.getElementById('ctl00_ContentBody_FilterControl1_uxSearchLocation')){
+            if (document.getElementById('ctl00_ContentBody_FilterControl1_uxSearchLocation')) {
                 let btn = document.createElement('button');
-                btn.type='button';
-                btn.id='InsertMyLocationSearch';
-                btn.innerText='My Location';
-                btn.onclick=function(){
-                    navigator.geolocation.getCurrentPosition(function(loc){
-                        document.getElementById('ctl00_ContentBody_FilterControl1_uxSearchLocation').value=`N ${loc.coords.latitude}° E ${loc.coords.longitude}°`;
+                btn.type = 'button';
+                btn.id = 'InsertMyLocationSearch';
+                btn.innerText = 'My Location';
+                btn.onclick = function () {
+                    navigator.geolocation.getCurrentPosition(function (loc) {
+                        document.getElementById('ctl00_ContentBody_FilterControl1_uxSearchLocation').value = `N ${loc.coords.latitude}° E ${loc.coords.longitude}°`;
                     });
                 }
             }
 
             //My Location Button(Scavenger Hunt Search)
-            if(document.getElementById('ctl00_ContentBody_NewSearchControl1_txtGames')){
+            if (document.getElementById('ctl00_ContentBody_NewSearchControl1_txtGames')) {
                 let input = document.getElementById('ctl00_ContentBody_NewSearchControl1_txtGames');
                 let btn = document.createElement('button');
-                btn.type='button';
-                btn.id='InsertMyLocationScaveng';
-                btn.innerText='My Location';
-                btn.onclick=function(){
-                    navigator.geolocation.getCurrentPosition(function(loc){
-                        input.value=`N ${loc.coords.latitude}° E ${loc.coords.longitude}°`;
+                btn.type = 'button';
+                btn.id = 'InsertMyLocationScaveng';
+                btn.innerText = 'My Location';
+                btn.onclick = function () {
+                    navigator.geolocation.getCurrentPosition(function (loc) {
+                        input.value = `N ${loc.coords.latitude}° E ${loc.coords.longitude}°`;
                     });
                 };
                 input.parentElement.insertBefore(btn, document.getElementById('ctl00_ContentBody_NewSearchControl1_uxGameTextValidator'));
             }
 
             //My Location Button (Homepage etc.)
-            if(document.getElementById('ctl00_ContentBody_NewSearchControl1_btnSearch')){
+            if (document.getElementById('ctl00_ContentBody_NewSearchControl1_btnSearch')) {
                 let btn = document.createElement('button');
-                btn.type='button';
-                btn.id='InsertMyLocationWaym';
-                btn.innerText='My Location';
-                btn.onclick=function(){
-                    navigator.geolocation.getCurrentPosition(function(loc){
-                        document.getElementById('ctl00_ContentBody_NewSearchControl1_txtAddress').value=`N ${loc.coords.latitude}° E ${loc.coords.longitude}°`;
+                btn.type = 'button';
+                btn.id = 'InsertMyLocationWaym';
+                btn.innerText = 'My Location';
+                btn.onclick = function () {
+                    navigator.geolocation.getCurrentPosition(function (loc) {
+                        document.getElementById('ctl00_ContentBody_NewSearchControl1_txtAddress').value = `N ${loc.coords.latitude}° E ${loc.coords.longitude}°`;
                     });
                 };
                 document.getElementById('search_addresstextbox').insertBefore(btn, document.getElementById('search_additional'));
@@ -191,21 +198,21 @@ var extender = {
 
         //do a custom header
         var hdr = document.createElement('div');
-        hdr.id='mobileHeader';
+        hdr.id = 'mobileHeader';
         //Nav Menu Button
         let toggleNav = document.createElement('button');
-        toggleNav.type='button';
-        toggleNav.id='mobileNavBtn';
+        toggleNav.type = 'button';
+        toggleNav.id = 'mobileNavBtn';
         toggleNav.classList.add('material-icons');
         let menuImg = document.createElement('img');
-        menuImg.src='https://github.com/google/material-design-icons/raw/master/png/navigation/menu/materialicons/48dp/2x/baseline_menu_black_48dp.png'
+        menuImg.src = 'https://github.com/google/material-design-icons/raw/master/png/navigation/menu/materialicons/48dp/2x/baseline_menu_black_48dp.png'
         toggleNav.append(menuImg);
-        toggleNav.onclick=function(){
-            if(document.getElementById('mobileNav').classList.length > 0){
+        toggleNav.onclick = function () {
+            if (document.getElementById('mobileNav').classList.length > 0) {
                 //show nav
                 document.getElementById('mobileNav').classList.remove('navHidden');
                 document.getElementById('AccountActions').classList.add('profileHidden');
-            } else{
+            } else {
                 //hide nav
                 document.getElementById('mobileNav').classList.add('navHidden');
             }
@@ -214,28 +221,28 @@ var extender = {
 
         //Logo
         let homelink = document.createElement('a');
-        homelink.href="/";
+        homelink.href = "/";
         let logo = document.createElement('img');
-        logo.src='/images/homepage/biglogo.gif';
-        logo.id='mobileLogo';
+        logo.src = '/images/homepage/biglogo.gif';
+        logo.id = 'mobileLogo';
         homelink.append(logo);
         hdr.append(homelink);
         document.getElementById('wrap').prepend(hdr);
 
         //Account details / login Button
         let accBtn = document.createElement('button');
-        accBtn.type='button';
+        accBtn.type = 'button';
         accBtn.classList.add('material-icons');
-        accBtn.id='mobileProfile';
+        accBtn.id = 'mobileProfile';
         let accImg = document.createElement('img');
-        accImg.src='https://github.com/google/material-design-icons/raw/master/png/action/account_circle/materialicons/48dp/2x/baseline_account_circle_black_48dp.png';
+        accImg.src = 'https://github.com/google/material-design-icons/raw/master/png/action/account_circle/materialicons/48dp/2x/baseline_account_circle_black_48dp.png';
         accBtn.append(accImg);
-        accBtn.onclick=function(){
-            if(document.getElementById('AccountActions').classList.length > 0){
+        accBtn.onclick = function () {
+            if (document.getElementById('AccountActions').classList.length > 0) {
                 //show account actions
                 document.getElementById('AccountActions').classList.remove('profileHidden');
                 document.getElementById('mobileNav').classList.add('navHidden');
-            } else{
+            } else {
                 //hide account actions
                 document.getElementById('AccountActions').classList.add('profileHidden');
             }
@@ -244,9 +251,9 @@ var extender = {
 
         //Nav Menu
         let navContainer = document.createElement('ul');
-        navContainer.id='mobileNav';
+        navContainer.id = 'mobileNav';
         let navElems = document.querySelectorAll('#main_nav li');
-        for(let i = 0; i < navElems.length; i++){
+        for (let i = 0; i < navElems.length; i++) {
             let newElem = navElems[i].cloneNode(true);
             newElem.removeAttribute('id');
             navContainer.prepend(newElem);
@@ -254,24 +261,25 @@ var extender = {
         hdr.append(navContainer);
 
         //Account Details/Login List
-        let accList=document.createElement('ul');
-        accList.id='AccountActions';
-        if(document.getElementById('ctl00_HomePageHeaderControl1_lnkLoginName') || document.getElementById('ctl00_HeaderControl1_lnkLoginName')){
+        let accList = document.createElement('ul');
+        accList.id = 'AccountActions';
+        if (document.getElementById('ctl00_HomePageHeaderControl1_lnkLoginName') || document.getElementById('ctl00_HeaderControl1_lnkLoginName')) {
             let profItem = document.createElement('li');
             let profLink = document.createElement('a');
-            profLink.href='/users/profile.aspx';
-            profLink.innerText='My Profile';
+            profLink.href = '/users/profile.aspx';
+            profLink.innerText = 'My Profile';
             profItem.append(profLink);
             accList.append(profItem);
         }
         let loginItem = document.createElement('li');
         let loginLink = document.createElement('a');
-        if(document.getElementById('ctl00_HomePageHeaderControl1_lnkLoginName') || document.getElementById('ctl00_HeaderControl1_lnkLoginName')){
-            loginLink.href='/login/default.aspx?RESET=Y&redir='+window.location.pathname;
-            loginLink.innerText='Log out';}else{
-                loginLink.href='/login/default.aspx?redir='+window.location.pathname;
-                loginLink.innerText='Log in';
-            }
+        if (document.getElementById('ctl00_HomePageHeaderControl1_lnkLoginName') || document.getElementById('ctl00_HeaderControl1_lnkLoginName')) {
+            loginLink.href = '/login/default.aspx?RESET=Y&redir=' + window.location.pathname;
+            loginLink.innerText = 'Log out';
+        } else {
+            loginLink.href = '/login/default.aspx?redir=' + window.location.pathname;
+            loginLink.innerText = 'Log in';
+        }
         loginItem.append(loginLink);
         accList.append(loginItem);
         hdr.append(accList);
@@ -280,22 +288,22 @@ var extender = {
 
         //Sidebar hide Button
         var hideSidebar = document.createElement('button');
-        hideSidebar.innerText='<';
-        hideSidebar.type='button';
-        hideSidebar.id='toggleSidebar';
-        hideSidebar.onclick=function(){
-            if(document.getElementById('sidebar').classList.length > 0){
+        hideSidebar.innerText = '<';
+        hideSidebar.type = 'button';
+        hideSidebar.id = 'toggleSidebar';
+        hideSidebar.onclick = function () {
+            if (document.getElementById('sidebar').classList.length > 0) {
                 // show the sidebar
                 document.getElementById('sidebar').classList.remove('sidebarHidden');
-                hideSidebar.innerText='>';
+                hideSidebar.innerText = '>';
             }
-            else{
+            else {
                 //hide sidebar
                 document.getElementById('sidebar').classList.add('sidebarHidden');
-                hideSidebar.innerText='<';
+                hideSidebar.innerText = '<';
             }
         };
-        document.getElementById('sidebar').insertBefore(hideSidebar,document.getElementById('sidebar').firstElementChild);
+        document.getElementById('sidebar').insertBefore(hideSidebar, document.getElementById('sidebar').firstElementChild);
 
         //hide all elements by default.
         document.getElementById('sidebar').classList.add('sidebarHidden');
@@ -305,23 +313,23 @@ var extender = {
 
 };
 
-function getTotalOffsetLeft(element){
+function getTotalOffsetLeft(element) {
     return element.getBoundingClientRect().left + window.pageXOffset;
 }
 
-function updateMaxWidth(){
-let elems = document.querySelectorAll('#wrap :not(br, style, #toggleSidebar, #mobileHeader, #mobileHeader *, #sidebar)');
-        elems.forEach(function(elem){
-            let offset = getTotalOffsetLeft(elem);
-            if(offset >= document.documentElement.clientWidth){
-                elem.parentElement.insertBefore(document.createElement('br'),elem);
-                offset = getTotalOffsetLeft(elem);
-            }
-            elem.style['max-width']=`calc(100vw - 1.1em - ${offset}px)`;
-        });
+function updateMaxWidth() {
+    let elems = document.querySelectorAll('#wrap :not(br, style, #toggleSidebar, #mobileHeader, #mobileHeader *, #sidebar)');
+    elems.forEach(function (elem) {
+        let offset = getTotalOffsetLeft(elem);
+        if (offset >= document.documentElement.clientWidth) {
+            elem.parentElement.insertBefore(document.createElement('br'), elem);
+            offset = getTotalOffsetLeft(elem);
+        }
+        elem.style['max-width'] = `calc(100vw - 1.1em - ${offset}px)`;
+    });
 }
 
-(function() {
-    window.addEventListener('load',extender.setup);
+(function () {
+    window.addEventListener('load', extender.setup);
     window.addEventListener('resize', updateMaxWidth);
 })();
