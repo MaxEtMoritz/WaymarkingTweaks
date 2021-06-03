6e1951ad57425a74d2e75f597a2658b55dafaa38
MaxEtMoritz <markus@memotech.franken.de>
2021-06-03 18:24:07 +0200

    updates

--- Waymarking Tweaks.user.js
+++ Waymarking Tweaks.user.js
@@ -8,13 +8,187 @@
 // @icon         https://www.waymarking.com/images/homepage/biglogo.gif
 // @updateURL    https://github.com/MaxEtMoritz/WaymarkingTweaks/raw/main/Waymarking%20Tweaks.user.js
 // @downloadURL  https://github.com/MaxEtMoritz/WaymarkingTweaks/raw/main/Waymarking%20Tweaks.user.js
-// @run-at       document-end
 // @grant        none
-// @resource     css /raw/main/WMTweaks.css
 // ==/UserScript==
 
 var extender = {
     setup: function(){
+        var css = document.createElement('style');
+    css.textContent=`
+    #sidebar.sidebarHidden .gutter {
+        display: none;
+      }
+      #sidebar .gutter {
+        grid-column: 2;
+        overflow-x:scroll;
+      }
+      #sidebar {
+        position: fixed;
+        right: 0;
+        display: grid;
+        height: 100%;
+        width: auto;
+        min-width: unset;
+        margin-top: 6vmin;
+        max-width: min(90vw, 370px);
+      }
+      #toggleSidebar {
+        grid-column: 1;
+        width: 1.1em;
+        padding: 0;
+        background: linear-gradient(#56849b,lightblue);
+      }
+      #content {
+        width: calc(100vw - 1.1em);
+        margin-top: 6vmin;
+      }
+      #wrap {
+        max-width: unset;
+        min-width: unset;
+      }
+      .bannerimage {
+        display: none;
+      }
+      #header {
+        display: none;
+      }
+      #searchtabs {
+        position: inherit;
+      }
+      #mobileHeader {
+        width: 100%;
+        position: fixed;
+        background-color: #56849b;
+        max-height:100vh;
+        overflow:scroll;
+      }
+      #mobileNav.navHidden li {
+        display: none;
+      }
+      #mobileHeader ul {
+        margin-block: 0;
+        list-style: none;
+        padding-inline: 0;
+        text-align: center;
+      }
+      #mobileHeader ul li {
+        margin: 10px;
+      }
+      #mobileHeader ul li a {
+        display: block;
+        padding: 1vw;
+        color: #333;
+        text-decoration: none;
+        border: 1px solid #ccc;
+        background: #f9e9a9 url(/images/blue.gif) repeat-x top left;
+      }
+      #mobileHeader ul li a:hover {
+        color: #333;
+        border-color: #ccc;
+        background: #fff url(/images/yellow.gif) repeat-x top left;
+      }
+      #mobileLogo,
+      #mobileProfile img,
+      #mobileNavBtn img {
+        max-width: 100%;
+        max-height: 100%;
+        height: 6vmin;
+      }
+      #mobileProfile {
+        float: right;
+        margin-right: 10px;
+        padding: 0;
+      }
+      #mobileNavBtn {
+        margin-left: 10px;
+        margin-right: 41vw;
+        padding: 0;
+      }
+      #mobileHeader button {
+        color: #333;
+        text-decoration: none;
+        border: 1px solid #ccc;
+        background: #f9e9a9 url(/images/blue.gif) repeat-x top left;
+      }
+      #AccountActions.profileHidden > li {
+        display: none;
+      }
+      #search_addresstextbox {
+        position: inherit;
+      }
+      .home-banner {
+        display: none;
+      }
+      #pushwrapper{
+      min-width:unset;
+      }
+      #footerlinks {
+        width: auto;
+      }
+      body {
+        padding: 0px;
+        width: 100vw;
+      }
+      *{
+      min-width:unset;
+      overflow-wrap:break-word;
+      }
+      #ctl00_ContentBody_HuntUserGridControl1_uxGrid *{
+      margin:0;
+      padding:0;
+      width:auto;
+      font-weight:normal;
+      font-size:inherit;
+      }
+      `;
+    document.head.append(css);
+        if(navigator.geolocation){
+            //My Location Button(Waymark Search Filter)
+            if(document.getElementById('ctl00_ContentBody_FilterControl1_uxSearchLocation')){
+                let btn = document.createElement('button');
+                btn.type='button';
+                btn.id='InsertMyLocationSearch';
+                btn.innerText='My Location';
+                btn.onclick=function(){
+                    navigator.geolocation.getCurrentPosition(function(loc){
+                        document.getElementById('ctl00_ContentBody_FilterControl1_uxSearchLocation').value=`N ${loc.coords.latitude}° E ${loc.coords.longitude}°`;
+                    });
+                }
+            }
+
+            //My Location Button(Scavenger Hunt Search)
+            if(document.getElementById('ctl00_ContentBody_NewSearchControl1_txtGames')){
+                let input = document.getElementById('ctl00_ContentBody_NewSearchControl1_txtGames');
+                let btn = document.createElement('button');
+                btn.type='button';
+                btn.id='InsertMyLocationScaveng';
+                btn.innerText='My Location';
+                btn.onclick=function(){
+                    navigator.geolocation.getCurrentPosition(function(loc){
+                        input.value=`N ${loc.coords.latitude}° E ${loc.coords.longitude}°`;
+                    });
+                };
+                input.parentElement.insertBefore(btn, document.getElementById('ctl00_ContentBody_NewSearchControl1_uxGameTextValidator'));
+            }
+
+            //My Location Button (Homepage etc.)
+            if(document.getElementById('ctl00_ContentBody_NewSearchControl1_btnSearch')){
+                let btn = document.createElement('button');
+                btn.type='button';
+                btn.id='InsertMyLocationWaym';
+                btn.innerText='My Location';
+                btn.onclick=function(){
+                    navigator.geolocation.getCurrentPosition(function(loc){
+                        document.getElementById('ctl00_ContentBody_NewSearchControl1_txtAddress').value=`N ${loc.coords.latitude}° E ${loc.coords.longitude}°`;
+                    });
+                };
+                document.getElementById('search_addresstextbox').insertBefore(btn, document.getElementById('search_additional'));
+            }
+        }
+
+        //set max width to prevent overflow
+        updateMaxWidth();
+
         //do a custom header
         var hdr = document.createElement('div');
         hdr.id='mobileHeader';
@@ -104,11 +278,6 @@ var extender = {
 
         //TODO Heading!
 
-        //hide all elements by default.
-        document.getElementById('sidebar').classList.add('sidebarHidden');
-        document.getElementById('AccountActions').classList.add('profileHidden');
-        document.getElementById('mobileNav').classList.add('navHidden');
-
         //Sidebar hide Button
         var hideSidebar = document.createElement('button');
         hideSidebar.innerText='<';
@@ -128,68 +297,31 @@ var extender = {
         };
         document.getElementById('sidebar').insertBefore(hideSidebar,document.getElementById('sidebar').firstElementChild);
 
-        //My Location Button (Homepage etc.)
-        if(document.getElementById('ctl00_ContentBody_NewSearchControl1_btnSearch')){
-            let btn = document.createElement('button');
-            btn.type='button';
-            btn.id='InsertMyLocationWaym';
-            btn.innerText='My Location';
-            btn.onclick=function(){
-                if(navigator.geolocation){
-                    navigator.geolocation.getCurrentPosition(function(loc){
-                        document.getElementById('ctl00_ContentBody_NewSearchControl1_txtAddress').value=`N ${loc.coords.latitude}° E ${loc.coords.longitude}°`;
-                    });
-                }
-                else{
-                    btn.innerText='Not supported';
-                }
-            };
-            document.getElementById('search_addresstextbox').insertBefore(btn, document.getElementById('search_additional'));
-        }
-
-        //My Location Button(Waymark Search Filter)
-        if(document.getElementById('ctl00_ContentBody_FilterControl1_uxSearchLocation')){
-            let btn = document.createElement('button');
-            btn.type='button';
-            btn.id='InsertMyLocationSearch';
-            btn.innerText='My Location';
-            btn.onclick=function(){
-                if(navigator.geolocation){
-                    navigator.geolocation.getCurrentPosition(function(loc){
-                        document.getElementById('ctl00_ContentBody_FilterControl1_uxSearchLocation').value=`N ${loc.coords.latitude}° E ${loc.coords.longitude}°`;
-                    });
-                }
-                else{
-                    btn.innerText='Not supported';
-                }
-            };
-
-            //My Location Button(Scavenger Hunt Search)
-            document.getElementById('ctl00_ContentBody_FilterControl1_uxLocationPanel').insertBefore(btn, document.getElementById('search_additional'));}
-        if(document.getElementById('ctl00_ContentBody_NewSearchControl1_txtGames')){
-            let input = document.getElementById('ctl00_ContentBody_NewSearchControl1_txtGames');
-            let btn = document.createElement('button');
-            btn.type='button';
-            btn.id='InsertMyLocationScaveng';
-            btn.innerText='My Location';
-            btn.onclick=function(){
-                if(navigator.geolocation){
-                    navigator.geolocation.getCurrentPosition(function(loc){
-                        input.value=`N ${loc.coords.latitude}° E ${loc.coords.longitude}°`;
-                    });
-                }
-                else{
-                    btn.innerText='Not supported';
-                }
-            };
-            input.parentElement.insertBefore(btn, document.getElementById('ctl00_ContentBody_NewSearchControl1_uxGameTextValidator'));
-        }
+        //hide all elements by default.
+        document.getElementById('sidebar').classList.add('sidebarHidden');
+        document.getElementById('AccountActions').classList.add('profileHidden');
+        document.getElementById('mobileNav').classList.add('navHidden');
     }
+
 };
+
+function getTotalOffsetLeft(element){
+    return element.getBoundingClientRect().left + window.pageXOffset;
+}
+
+function updateMaxWidth(){
+let elems = document.querySelectorAll('#wrap :not(br, style, #toggleSidebar, #mobileHeader, #mobileHeader *, #sidebar)');
+        elems.forEach(function(elem){
+            let offset = getTotalOffsetLeft(elem);
+            if(offset >= document.documentElement.clientWidth){
+                elem.parentElement.insertBefore(document.createElement('br'),elem);
+                offset = getTotalOffsetLeft(elem);
+            }
+            elem.style['max-width']=`calc(100vw - 1.1em - ${offset}px)`;
+        });
+}
 
 (function() {
     window.addEventListener('load',extender.setup);
-    var css = document.createElement('style');
-    css.textContent=GM.getResourceText('css');
-    document.head.append(css);
+    window.addEventListener('resize', updateMaxWidth);
 })();
